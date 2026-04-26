24a53e8f8f33583a29b68953a500867fedf21947
Hemang-codes <hemangkawa.in@gmail.com>
2026-04-26 14:11:15 +0530

    Re-Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,8 @@
 
 # Configuration
 DISK="/dev/sda"      
-# NOTE: Tiny11 created 3 partitions (EFI, MSR, Windows). 
-# So your Arch space is actually partition 4.
-ARCH_PART="${DISK}4" 
+# UPDATED: Using sda5 based on your last lsblk photo!
+ARCH_PART="${DISK}5" 
 USER_NAME="hemang"
 REPO="https://github.com/Hemang-codes/myArchy.git"
 
@@ -14,7 +13,7 @@ mount $ARCH_PART /mnt
 mkdir -p /mnt/boot/efi
 mount "${DISK}1" /mnt/boot/efi # Reusing your 512MB EFI partition
 
-# 2. Pacstrap (Added essential drivers and boot tools)
+# 2. Pacstrap
 pacstrap /mnt base linux linux-firmware intel-ucode git nvim sudo grub efibootmgr os-prober networkmanager ntfs-3g
 
 # 3. Generate fstab
@@ -26,6 +25,7 @@ ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
 hwclock --systohc
 echo "arch-hemang" > /etc/hostname
 useradd -m -G wheel $USER_NAME
+# REMEMBER: Change 'password' to your actual password after installation!
 echo "$USER_NAME:password" | chpasswd
 echo "%wheel ALL=(ALL:ALL) ALL" >> /etc/sudoers
 
@@ -37,7 +37,7 @@ echo "GRUB_DISABLE_OS_PROBER=false" >> /etc/default/grub
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=ArchLinux
 grub-mkconfig -o /boot/grub/grub.cfg
 
-# Pre-clone the repo
+# Pre-clone the repo for your GNU Stow setup
 git clone $REPO /home/$USER_NAME/myArchy
 chown -R $USER_NAME:$USER_NAME /home/$USER_NAME/myArchy
 EOF
