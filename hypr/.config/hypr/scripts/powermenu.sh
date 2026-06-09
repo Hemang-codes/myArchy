637c3a8b0dcdb2c2da333f6628105ea7e703fe8e
Hemang-codes <hemangkawa.in@gmail.com>
2026-06-09 21:44:24 +0530

    Re-config

--- hypr/.config/hypr/scripts/powermenu.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-choice=$(echo -e "Lock\nLogout\nSuspend\nReboot\nShutdown" | rofi -dmenu -p "Power Menu")
-case $choice in
-Lock) hyprlock ;; # Or swaylock
-Logout) hyprctl dispatch exit ;;
-Suspend) systemctl suspend ;;
-Reboot) systemctl reboot ;;
-Shutdown) systemctl poweroff ;;
-esac
